84924e3fd7b2aa574473011945bc0233f056a5d4
robhhr <robhhr@outlook.com>
2020-05-19 23:16:58 -0700

    add ubersicht config

---
+++ ubersicht/widgets/pecan/clock.jsx
@@ -0,0 +1,6 @@
+const command = 'date +"%I:%M"';
+const refreshFrequency = 60000; // ms
+
+const render = ({ output }) => <div class='screen'><div class='pecanclock'>{`${output}`}</div></div>;
+
+export { command, refreshFrequency, render };
