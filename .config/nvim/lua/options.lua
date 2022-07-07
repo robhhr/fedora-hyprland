c2f6834195262c9bc672df5fb1988d5be1fffe07
robhhr <robhhr@outlook.com>
2022-07-06 23:03:32 -0700

    refactor config lua

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,17 +1,23 @@
-vim.opt.number = true         -- show numbers
-vim.o.background = 'dark'     -- bg
-vim.opt.hlsearch = true       -- highlight search results
-vim.opt.tabstop = 2           -- tab size
-vim.opt.shiftwidth = 0        -- autoindent size
-vim.opt.smartindent = true     -- set smart indents
-vim.opt.expandtab = true      -- spaces
-vim.opt.swapfile = false      -- no backup files
-vim.opt.mouse = "i"           -- enable mouse on insert
-vim.opt.showmatch = true      -- highlights matching bracket
-vim.opt.termguicolors = true  -- for themes
-vim.opt.hidden = true         -- multiple buffers
-vim.opt.showmode = true       -- remove insert msg
-vim.opt.encoding = 'utf-8'    -- encoding
-vim.opt.cmdheight = 2         -- better msg readability
-vim.opt.scrolloff = 8         -- replace zz
-vim.opt.relativenumber = true -- relative
+local options = {
+  number = true,            -- show numbers
+  background = 'dark',      -- bg
+  hlsearch = true,          -- highlight search results
+  tabstop = 2,              -- tab size
+  shiftwidth = 0,           -- autoindent size
+  smartindent = true,       -- set smart indents
+  expandtab = true,         -- spaces
+  swapfile = false,         -- no backup files
+  mouse = "i",              -- enable mouse on insert
+  showmatch = true,         -- highlights matching bracket
+  termguicolors = true,     -- for themes
+  hidden = true,            -- multiple buffers
+  showmode = true,          -- add insert msg
+  encoding = 'utf-8',       -- encoding
+  cmdheight = 2,            -- better msg readability
+  scrolloff = 8,            -- replace zz
+  relativenumber = true    -- relative
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
