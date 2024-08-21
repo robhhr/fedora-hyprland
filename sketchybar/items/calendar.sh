e482bec5b1eebf9d805a6c96ec453d9988002dc8
robhhr <robberto@hey.com>
2024-08-21 10:14:48 -0700

    edit dimensions items

--- sketchybar/items/calendar.sh
+++ sketchybar/items/calendar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sketchybar --add item calendar right \
-           --set calendar icon=   \
-                          update_freq=30 \
-                          icon.color=0xffffff00 \
+           --set calendar update_freq=30 \
+                          label.padding_left=8 \
+                          label.padding_right=8 \
                           script="$PLUGIN_DIR/calendar.sh"
 
