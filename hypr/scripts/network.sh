16f8f52eb7e6fbc1eab219d2b388615f5b7e9292
robhhr <robertohhr@pm.me>
2025-03-22 16:39:14 -0700

    add network notify on start

--- hypr/scripts/network.sh
+++ hypr/scripts/network.sh
@@ -1,8 +1,30 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-if nmcli connection show --active | grep -q "wg0-mullvad"; then
-  echo ""
-else
-  echo ""
-fi
+for i in {1..10}; do
+  if pgrep -x dunst >/dev/null; then
+    break
+  fi
+  sleep 0.5
+done
+
+for i in {1..5}; do
+  JSON=$(mullvad status --json)
+
+  STATE=$(echo "$JSON" | jq -r '.state')
+
+  if [[ "$STATE" == "connected" ]]; then
+    COUNTRY=$(echo "$JSON" | jq -r '.details.location.country')
+    CITY=$(echo "$JSON" | jq -r '.details.location.city')
+    ADDRESS=$(echo "$JSON" | jq -r '.details.endpoint.address')
+    TUNNEL=$(echo "$JSON" | jq -r '.details.endpoint.tunnel_interface')
+
+    notify-send "${TUNNEL}" "connected to ${CITY}, ${COUNTRY}.\n${ADDRESS}"
+    exit 0
+  fi
+
+  sleep 1
+done
+
+notify-send "VPN" "⚠️ Mullvad is not connected!"
+exit 1
 
