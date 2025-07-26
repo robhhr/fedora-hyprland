8f9956d0f77cdca44d632288f641b1ea9e1f6e7e
robhhr <robertohhr@pm.me>
2025-07-26 01:18:27 -0700

    update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,7 @@ alias zshreload='source ~/.zshrc'
 alias grep='grep --color=auto'
 alias less='less -R'
 alias e='exit'
+alias lsl="lsd --tree -l"
 
 # paths
 alias ..='cd ..'
@@ -14,22 +15,17 @@ alias htdocs='~/../../Applications/MAMP/htdocs'
 alias wpl='~/Local\ Sites'
 alias config='~/.config'
 alias hconf='~/.config/hypr/'
+alias nconf='~/.config/nvim/'
 
 # git
 alias g='git'
 alias gA='git add -A'
-alias gU='git add -u'
 alias gc='git commit -m'
 alias gp='git push'
 alias gpo='git pull origin'
-alias gcb='git checkout -b'
 alias gs='git status'
-alias gsw='git switch'
-
-# nvim
-alias n='nvim'
-alias nconf='nvim ~/.config/nvim/'
-alias nzsh='nvim ~/.zshrc'
+alias gsc='git switch -c'
+alias gdel='git branch -d'
 
 # misc
 alias t='tmux'
